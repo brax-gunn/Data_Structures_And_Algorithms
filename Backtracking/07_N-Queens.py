024f0789ad1e9153ec514fedfd56a1adbf1ae367
brax-gunn <brax.gunn.99@gmail.com>
2022-07-16 13:42:54 +0530

    Add Problems of Backtracking

---
+++ Backtracking/07_N-Queens.py
@@ -0,0 +1,79 @@
+class Solution:
+    def solveNQueens(self, n: int) -> List[List[str]]:
+        answer = []
+        
+        chessBoard = []
+        for i in range(0, n):
+            temp = []
+            for j in range(0 , n):
+                temp.append('.')
+            chessBoard.append(temp)
+        
+        self.__recursionOnBoard(0, n, chessBoard, answer)
+        
+        return answer
+        
+    def __recursionOnBoard(self, currentRow, n, chessBoard, answer):
+        if currentRow >= n:
+            temp = []
+            for row in range(0, n):
+                tempStr = ''
+                for col in range(0, n):
+                    tempStr+=chessBoard[row][col]
+                temp.append(tempStr)
+                    
+            answer.append(temp.copy())
+            return
+        
+        
+        for col in range(0, n):
+            if self.__isPosValid(currentRow, col, n, chessBoard):
+                chessBoard[currentRow][col] = 'Q'
+                self.__recursionOnBoard(currentRow+1, n, chessBoard, answer)
+                chessBoard[currentRow][col] = '.'
+                
+        return
+
+    def __isPosValid(self, currentRow, currentCol, n, chessBoard):
+        # check in row
+        for pos in range(0, n):
+            if chessBoard[currentRow][pos] == 'Q':
+                return False
+        # check in column
+        for pos in range(0, n):
+            if chessBoard[pos][currentCol] == 'Q':
+                return False
+        # for diagonal top left
+        currentX = currentRow
+        currentY = currentCol
+        while currentX >= 0 and currentY >= 0:
+            if chessBoard[currentX][currentY] == 'Q':
+                return False
+            currentX -= 1
+            currentY -= 1
+        # for diagonal top right
+        currentX = currentRow
+        currentY = currentCol
+        while currentX >= 0 and currentY < n:
+            if chessBoard[currentX][currentY] == 'Q':
+                return False
+            currentX -= 1
+            currentY += 1
+        # for diagonal bottom left
+        currentX = currentRow
+        currentY = currentCol
+        while currentX < n and currentY >= 0:
+            if chessBoard[currentX][currentY] == 'Q':
+                return False
+            currentX += 1
+            currentY -= 1
+        # for diagonal bottom right
+        currentX = currentRow
+        currentY = currentCol
+        while currentX < n and currentY < n:
+            if chessBoard[currentX][currentY] == 'Q':
+                return False
+            currentX += 1
+            currentY += 1
+        
+        return True
--- README.md
+++ README.md
@@ -10,4 +10,6 @@
 | 4     | Subsets                   | **MEDIUM** | https://leetcode.com/problems/subsets/   |
 | 5     | Combination Sum           | **MEDIUM** | https://leetcode.com/problems/combination-sum/ |
 | 6     | Rat in a Maze Problem - I | **MEDIUM** | https://practice.geeksforgeeks.org/problems/rat-in-a-maze-problem/1 |
+| 7     | N-Queens                  |  **HARD**  | https://leetcode.com/problems/n-queens/  |
+| 8     | N-Queens II               |  **HARD**  | https://leetcode.com/problems/n-queens-ii/ |
 
